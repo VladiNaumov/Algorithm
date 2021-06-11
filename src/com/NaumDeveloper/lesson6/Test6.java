08c215d1a27a8c7584ce66a274c849a1e9e72f2b
Sipmsons <umbrella.spb@hotmail.com>
2021-06-11 19:24:25 +0300

    Algorithms

---
+++ src/com/NaumDeveloper/lesson6/Test6.java
@@ -0,0 +1,33 @@
+package com.NaumDeveloper.lesson6;
+
+public class Test6 {
+
+    public static void main(String[] args) {
+        ITree<Integer> myTree = new MyTree<>();
+        myTree.add(60);
+        myTree.add(50);
+        myTree.add(66);
+        myTree.add(40);
+        myTree.add(55);
+        myTree.add(70);
+        myTree.add(31);
+        myTree.add(45);
+        myTree.add(42);
+        myTree.add(43);
+        myTree.add(67);
+        myTree.add(81);
+
+        System.out.println("Find 70: " + myTree.contains(70));
+        System.out.println("Find 700: " + myTree.contains(700));
+
+
+    //   myTree.traverse(ITree.TraverseMode.IN_ORDER);
+
+        myTree.display();
+        myTree.remove(55);
+//      myTree.remove(42);
+        myTree.remove(60);
+        myTree.display();
+
+    }
+}
